74caaa42e464eb89105ccd44c975ddb725db449a
Paintballskaguy <Johnstournament@gmail.com>
2025-06-02 15:42:24 -0500

    final task. couldn't get it to load correctly.

--- localization/app.py
+++ localization/app.py
@@ -5,7 +5,7 @@ from flask_babel import Babel, _, ngettext
 
 app = Flask(__name__)
 
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_LOCALE'] = 'en' 'fr'
 app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'translations'
 
 babel = Babel(app)
@@ -17,8 +17,8 @@ def home():
 
     message_count = 3
     messages = ngettext(
-        "You have one message", 
-        "You have %(num)d messages", 
+        "You have one message",
+        "You have %(num)d messages",
         message_count
     ) % {'num': message_count}
 
